171e39b0abf86e76a9d510649ff672dc7c48a07d
imjarik <tank18190@gmail.com>
2025-05-20 09:50:18 +0300

    JARIK

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Ukrainian"); // Set the locale to Ukrainian
+
+    //int num = 45;
+    //std::cout << "number" << num <<std::endl; 
+
+    int a, b;
+    std::cout << "Введітьа a ";  
+    std::cin >> a;
+
+    std::cout << "Введіть  b ";
+    std:cin >> b;
+    std::cout << "a = " << a << " b = " << b <<endl;   
+    
+
+   
+    
+    return 0;
+}   
